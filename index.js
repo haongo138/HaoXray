92c35ab93597aa894625ff664a4230f861fea47d
Hao Ngo <hyosimple@gmail.com>
2019-03-31 09:13:44 +0700

    add .env & dotenv module

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const userRoute = require('./routes/user.route');
 const authRoute = require('./routes/auth.route');
+const dotenv = require('dotenv');
 
-const port = 3000;
+//setup environment
+dotenv.config();
 
+//create Express app
 const app = express();
+
+//config view engine
 app.set('view engine', 'pug');
 app.set('views', './views');
 
@@ -14,6 +20,7 @@ app.use(bodyParser.json()); //for parsing  application/json
 app.use(bodyParser.urlencoded({ extend: true})); //for parsing application/x-www-form-urlencoded
 app.use(express.static('public'));
 app.use(cookieParser());
+
 app.get('/', (req,res) => res.render('index', {
   username: 'toxicFumo'
 }));
@@ -21,4 +28,7 @@ app.get('/', (req,res) => res.render('index', {
 app.use('/users', userRoute);
 app.use('/auth', authRoute); 
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const PORT = process.env.PORT || 5000;
+
+//run server
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
