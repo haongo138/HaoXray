56085a2c2c0745d09325fc801bcdca5422891925
Hao Ngo <hyosimple@gmail.com>
2019-04-01 16:46:37 +0700

    Re-config route & add comment

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const userRoute = require('./routes/user.route');
-const authRoute = require('./routes/auth.route');
 const dotenv = require('dotenv');
 
-//setup environment
+// routes 
+const userRoute = require('./routes/user.route');
+const authRoute = require('./routes/auth.route');
+const indexRoute = require('./routes/index.route');
+
+// setup environment
 dotenv.config();
 
-//create Express app
+// create Express app
 const app = express();
 
-//config view engine
+// config view engine
 app.set('view engine', 'pug');
 app.set('views', './views');
 
@@ -21,14 +23,13 @@ app.use(bodyParser.urlencoded({ extend: true})); //for parsing application/x-www
 app.use(express.static('public'));
 app.use(cookieParser());
 
-app.get('/', (req,res) => res.render('index', {
-  username: 'toxicFumo'
-}));
-
+// setup routes
+app.use('/', indexRoute);
 app.use('/users', userRoute);
 app.use('/auth', authRoute); 
 
+// assign run port
 const PORT = process.env.PORT || 5000;
 
-//run server
+// run server
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,14 +1,14 @@
-const express = require('express');
-
-const router = express.Router();
+const router = require('express').Router();
 const controller = require('../controllers/user.controller');
 const validate = require('../validate/user.validate');
 const authMiddleware = require('../middlewares/auth.middleware');
+
 // GET
 router.get('/', authMiddleware.requireAuth, controller.index);
 router.get('/search', authMiddleware.requireAuth, controller.search);
 router.get('/register', controller.register);
 router.get('/:id', authMiddleware.requireAuth, controller.getUserInfo);
+
 // POST
 router.post('/register', validate.postRegister, controller.postRegister);
 
